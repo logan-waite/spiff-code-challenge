b5e28ce5d02a21ecd0a8447d296d79967d0f4b78
Logan Waite <lwaite2010@gmail.com>
2023-02-15 21:40:29 -0500

    Some last minute refactoring

--- src/progress_bar_exercise/async_progress_bar/AsyncProgressBar.js
+++ src/progress_bar_exercise/async_progress_bar/AsyncProgressBar.js
@@ -11,48 +11,50 @@ export default function AsyncProgressBar({
   }
 
   const [progress, setProgress] = useState(0);
-  const [fadeOut, setFadeOut] = useState(false);
   const [slowCounter, setSlowCounter] = useState(0);
 
   useEffect(() => {
-    let progressInterval;
-    if (requestState === "started") {
-      setFadeOut(false);
-
-      progressInterval = setInterval(() => {
-        if (progress <= 90) {
-          if (breakpoints.includes(progress + 2) || slowCounter > 0) {
-            setSlowCounter(slowCounter + 1);
-            setProgress(progress + 0.1);
-            if (slowCounter >= 20) {
-              setSlowCounter(0);
-              setProgress(Math.ceil(progress)); // to fix floating point issues.
-            }
-          } else {
-            setProgress(progress + 1);
-          }
-        }
-      }, 100);
-    } else if (requestState === "finished") {
-      setProgress(100);
-      clearInterval(progressInterval);
-      // let the progress bar finish animating to the end.
-      setTimeout(() => {
-        setFadeOut(true);
-      }, 1000);
-
-      // call onFinished and reset the progress to 0 after the bar fades out.
-      setTimeout(() => {
-        onFinished();
-        setProgress(0);
-      }, 4000); // 1s for finishing, 2s wait, 1s fadeout
-
-      clearInterval(progressInterval);
+    function incrementProgressSlowly() {
+      setSlowCounter(slowCounter + 1);
+      setProgress(progress + 0.1);
+      if (slowCounter >= 20) {
+        setSlowCounter(0);
+        setProgress(Math.round(progress)); // to fix floating point issues.
+      }
     }
+
+    let progressInterval;
+    switch (requestState) {
+      case "started":
+        progressInterval = setInterval(() => {
+          if (progress <= 90) {
+            if (breakpoints.includes(progress + 1) || slowCounter > 0) {
+              incrementProgressSlowly();
+            } else {
+              setProgress(progress + 1);
+            }
+          }
+        }, 100);
+        break;
+      case "finished":
+        setProgress(100);
+        clearInterval(progressInterval);
+        break;
+      default:
+        setProgress(0);
+        setSlowCounter(0);
+        clearInterval(progressInterval);
+    }
+
     return () => clearInterval(progressInterval);
   }, [progress, breakpoints, slowCounter, onFinished, requestState]);
 
+  function handleFinish() {
+    setProgress(0);
+    onFinished();
+  }
+
   return requestState !== "inactive" ? (
-    <ProgressBar progress={progress} fadeOut={fadeOut}></ProgressBar>
+    <ProgressBar progress={progress} onFinish={handleFinish}></ProgressBar>
   ) : null;
 }
--- src/progress_bar_exercise/progress_bar/ProgressBar.js
+++ src/progress_bar_exercise/progress_bar/ProgressBar.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export function ProgressBar({ progress, fadeOut }) {
+export function ProgressBar({ progress, onFinish }) {
+  const [fadeOut, setFadeOut] = useState(false);
+
   const percent = progress > 100 ? 100 : progress < 0 ? 0 : progress;
   // get viewport width
   const vw = Math.max(
@@ -10,6 +12,20 @@ export function ProgressBar({ progress, fadeOut }) {
   // calculate the amount that needs to be hidden
   const percentHidden = 100 - percent;
   const hiddenAmount = (percentHidden * vw) / 100;
+
+  useEffect(() => {
+    if (percent === 100) {
+      // let the progress bar finish animating to the end.
+      setTimeout(() => {
+        setFadeOut(true);
+      }, 1000);
+
+      setTimeout(() => {
+        onFinish();
+        setFadeOut(false);
+      }, 4000); // 1s for finishing, 2s wait, 1s fadeout
+    }
+  }, [percent, onFinish]);
 
   return (
     <div
